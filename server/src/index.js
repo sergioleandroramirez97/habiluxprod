d49845fa5710fc877bad8a5b7c3ce71540a22cac
lramirez-leren <sergioleandroramirez97@gmail.com>
2025-11-26 09:12:45 -0300

    Cambio de listen

--- server/src/index.js
+++ server/src/index.js
@@ -39,6 +39,6 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
