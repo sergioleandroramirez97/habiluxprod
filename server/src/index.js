a993ed764d39613fcebe0c3ff760ce1af90a6cda
lramirez-leren <sergioleandroramirez97@gmail.com>
2025-11-27 22:31:08 -0300

    Cambios en el INDEX

--- server/src/index.js
+++ server/src/index.js
@@ -19,23 +19,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Configure CORS for production and development
-const allowedOrigins = [
-  'https://frontend-production-c901.up.railway.app',
-  'http://localhost:5173',
-  'http://localhost:3000',
-  'http://127.0.0.1:5173'
-];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: [
+    'https://frontend-production-c901.up.railway.app',
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'http://127.0.0.1:5173'
+  ],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   optionsSuccessStatus: 200
 }));
